8675e82a1c64d864eb4f85604d7843670a3f8078
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-10-16 14:40:14 +0300

    Added comments

--- question1_highest_average.py
+++ question1_highest_average.py
@@ -1,13 +1,13 @@
 #This is a program that takes in a list of discharge rates and returns the highest average
 
 def find_highest_average(discharge):
-    empty_list=[]  #create an empty list that will hold the sum of two contiguous rates 
+    new_list=[]  #create an empty list that will hold the sum of two contiguous rates 
     for rate in range(len(discharge)-1):
         high=discharge[rate]+discharge[rate+1]      
-        empty_list.append(high)                  #get the sum of every two contiguous rates and append it to the empty list
+        new_list.append(high)                  #get the sum of every two contiguous rates and append it to the empty list
 
-    highest_sum=empty_list[0]     #make an assumption that the highest sum is at the first index of the empty list
-    for i in empty_list:        
+    highest_sum=new_list[0]     #make an assumption that the highest sum is at the first index of the empty list
+    for i in new_list:        
         if i >=highest_sum:      #loop through the empty list and reasign the value of the highest sum 
             highest_sum=i
 
